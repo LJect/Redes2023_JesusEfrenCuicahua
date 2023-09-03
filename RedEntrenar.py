57933631abacf240de3500609e74a73ca33c8c47
LJect <jesus.efrenct@gmail.com>
2023-09-03 16:34:31 -0600

    se agregaron los archivos para entrenara la red y una captura de los resultados

---
+++ RedEntrenar.py
@@ -0,0 +1,6 @@
+import network1
+import mnist_loader1
+training_data, test_data, _ = mnist_loader1.load_data_wrapper();
+
+net=network1.Network([784,30,10]);
+net.SGD(training_data, 3 , 10, 0.01, test_data=test_data)
