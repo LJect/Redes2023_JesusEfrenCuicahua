1052d7e081a1768c0d86f39e2a25bdd06f884e7a
LJect <jesus.efrenct@gmail.com>
2023-09-11 22:45:07 -0600

    prueba de red entrenada con el gradiente con inercia

--- RedEntrenar.py
+++ RedEntrenar.py
@@ -3,4 +3,4 @@ import mnist_loader1
 training_data, test_data, _ = mnist_loader1.load_data_wrapper();
 
 net=network1.Network([784,30,10]);
-net.SGD(training_data, 30, 10, 0.6, 0.1, test_data=test_data);
+net.SGD(training_data, 30, 10, 0.01, 0.1, test_data=test_data);
